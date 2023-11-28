7dd0e28e33514aa496a64d2a5da6f12a6ccdc7fa
minu <wlwhsxz@gmail.com>
2023-11-28 23:15:06 +0900

    design: initial Login UI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup";
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="signup" element={<SignUp />} />
+          <Route path="signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Login.scss";
+
+export default function Login() {
+  return (
+    <div className="container">
+      <div className="login-box">
+        <img src="youpotify.png" alt="google logo" />
+        <div>
+          <h2>로그인</h2>
+        </div>
+        <form className="input">
+          <div>
+            <fieldset>
+              <legend>Email or Phone</legend>
+              <div>
+                <input id="email" placeholder="이메일" />
+              </div>
+            </fieldset>
+          </div>
+          <fieldset>
+            <legend>비밀번호 입력</legend>
+            <div>
+              <input id="password" placeholder="비밀번호" />
+            </div>
+          </fieldset>
+        </form>
+        <div className="btn-box">
+          <span id="signup">계정 만들기</span>
+          <span id="login">로그인</span>
+        </div>
+      </div>
+    </div>
+  );
+}
