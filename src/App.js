c2d297613291360136a9b23e74916a212589f02b
minu <wlwhsxz@gmail.com>
2023-12-04 21:46:40 +0900

    feat: 프로필 라우터 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import Profile from "./pages/Profile/Profile";
 
 export default function App() {
   return (
@@ -12,7 +13,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
