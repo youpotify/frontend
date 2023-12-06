12eb240e8964806f7337cd9aa10bc0c44c4fe43f
jhyjj <jhyj3768@gmail.com>
2023-12-06 15:50:21 +0900

    design: Artist ui 초안

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signu/Signup";
+
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path='/artist' element={<Artist/>}></Route>
+          <Route path='/album' element={<Album/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+
