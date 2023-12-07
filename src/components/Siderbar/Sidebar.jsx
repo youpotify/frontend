945cfd7df9e9481c8695c54af6e1b0357ab6104d
minu <wlwhsxz@gmail.com>
2023-12-07 23:20:52 +0900

    feat: sidebar 메뉴 초안

--- src/components/Siderbar/Sidebar.jsx
+++ src/components/Siderbar/Sidebar.jsx
@@ -3,27 +3,44 @@ import "./Sidebar.scss";
 
 export default function Sidebar() {
   const [isMenuClicked, setIsMenuClicked] = useState(false);
+
   return isMenuClicked ? (
     <div>
       <div className="sidebar-clicked">
         <div className="route-list">
+          <div
+            class="material-symbols-outlined"
+            id="menu"
+            onClick={() => setIsMenuClicked(false)}
+          >
+            menu
+          </div>
           <div>홈</div>
           <div>보관함</div>
           <div>추천곡</div>
-        </div>
-        <div className="playlist">
-          <div>새 재생목록</div>
-          <div>플레이 리스트 map 표현</div>
-          {/* drawer로 표현 */}
+          <div>+ 새 재생목록</div>
+          <div>좋아요 표시한 음악</div>
+          <div>
+            <div>재생목록1</div>
+            <div>재생목록2</div>
+            <div>재생목록3</div>
+            <div>재생목록4</div>
+          </div>
         </div>
       </div>
     </div>
   ) : (
     <div className="sidebar">
-      <span class="material-symbols-outlined">menu</span>
-      <div>홈</div>
-      <div>보관함</div>
-      <div>추천곡</div>
+      <div
+        class="material-symbols-outlined"
+        id="menu"
+        onClick={() => setIsMenuClicked(true)}
+      >
+        menu
+      </div>
+      <div class="material-symbols-outlined">home</div>
+      <div class="material-symbols-outlined">library_music</div>
+      <div class="material-symbols-outlined">recommend</div>
     </div>
   );
 }
