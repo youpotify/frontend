a274656e15e861d0d8795d52d1c4111a2bc3b50b
minu <wlwhsxz@gmail.com>
2023-12-25 01:37:23 +0900

    feat: youtube api - video 호출

--- src/components/Home/recommendations/music-video/MusicVideo.jsx
+++ src/components/Home/recommendations/music-video/MusicVideo.jsx
@@ -1,49 +1,49 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./MusicVideo.scss";
 
 export default function MusicVideo() {
+  const youtubeAPIKey = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+  const [data, setData] = useState([]);
+
+  const fetchVideos = () => {
+    axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=${youtubeAPIKey}`
+      )
+      .then((res) => {
+        setData(res.data.items);
+      })
+      .catch((error) => console.error("Error fetching data: ", error));
+  };
+
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+
   return (
     <div className="music-video">
       <h2>추천 뮤직비디오</h2>
       <div className="item-list">
-        <div className="item-container">
-          <iframe
-            id="ytplayer"
-            type="text/html"
-            width="360"
-            height="180"
-            src="https://www.youtube.com/embed/M7lc1UVf-VE"
-            frameborder="0"
-            allowfullscreen="allowfullscreen"
-          ></iframe>
-          <div id="video-title">타이틀</div>
-          <div id="video-owner">채널주인 - 조회수</div>
-        </div>
-        <div className="item-container">
-          <iframe
-            id="ytplayer"
-            type="text/html"
-            width="360"
-            height="180"
-            src="https://www.youtube.com/embed/M7lc1UVf-VE"
-            frameborder="0"
-            allowfullscreen="allowfullscreen"
-          ></iframe>
-          <div id="video-title">타이틀</div>
-          <div id="video-owner">채널주인 - 조회수</div>
-        </div>
-        <div className="item-container">
-          <iframe
-            id="ytplayer"
-            type="text/html"
-            width="360"
-            height="180"
-            src="https://www.youtube.com/embed/M7lc1UVf-VE"
-            frameborder="0"
-            allowfullscreen="allowfullscreen"
-          ></iframe>
-          <div id="video-title">타이틀</div>
-          <div id="video-owner">채널주인 - 조회수</div>
-        </div>
+        {data.map((e, index) => {
+          const src = `https://www.youtube.com/embed/${e.id}`;
+          return (
+            <div className="item-container" key={index}>
+              <iframe
+                id="ytplayer"
+                type="text/html"
+                width="320"
+                height="180"
+                src={src}
+                style={{ border: "none" }}
+                allowFullScreen
+              ></iframe>
+              <div>타이틀</div>
+              <div>영상 주인 - 조회 수</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
