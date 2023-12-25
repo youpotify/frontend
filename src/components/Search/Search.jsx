42b10db7a0e6a11c15b02e35737fbf8d7f95404d
minu <wlwhsxz@gmail.com>
2023-12-25 21:23:28 +0900

    feat: 검색 시 페이지 이동

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,20 +1,32 @@
 import "./Search.scss";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Search() {
+  const Navigate = useNavigate();
   const [keyword, setKeyword] = useState("");
 
   return (
     <div className="search">
-      <span class="material-symbols-outlined">search</span>
-      <span>
+      <span class="material-symbols-outlined" id="search-btn">
+        search
+      </span>
+      <form onSubmit={() => Navigate("/search")}>
         <input
           placeholder="노래, 앨범, 아티스트, 팟캐스트 검색"
           value={keyword}
           onChange={(e) => setKeyword(e.target.value)}
         />
-      </span>
-      <span class="material-symbols-outlined">close</span>
+      </form>
+      {keyword && (
+        <span
+          class="material-symbols-outlined"
+          id="delete-btn"
+          onClick={() => setKeyword("")}
+        >
+          close
+        </span>
+      )}
     </div>
   );
 }
