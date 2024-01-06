bee5d520274a597ed3148fbe7540afc0731113d5
minu <wlwhsxz@gmail.com>
2024-01-06 20:11:04 +0900

    fix: 중복 컴포넌트 적용으로 인한 ui 망가짐 현상 수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,22 +8,16 @@ import MusicVideo from "../../components/Home/recommendations/music-video/MusicV
 export default function Home() {
   return (
     <div className="home">
-      <Sidebar />
-      <div className="body">
-        <Header />
-        <div className="home_contents">
-          <HashTag />
-          {/* 해시태그에 다양한 종류의 플레이리스트들을 추천하는건? 
+      <HashTag />
+      {/* 해시태그에 다양한 종류의 플레이리스트들을 추천하는건? 
           my own ,forgotten ,weather ,mood, vibes,  */}
-          <MusicVideo />
+      <MusicVideo />
 
-          <div>추천 플레이리스트</div>
+      <div>추천 플레이리스트</div>
 
-          <div>잊고 있던 좋은 음악</div>
+      <div>잊고 있던 좋은 음악</div>
 
-          <div>인기 차트</div>
-        </div>
-      </div>
+      <div>인기 차트</div>
     </div>
   );
 }
